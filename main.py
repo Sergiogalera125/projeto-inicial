169660bf00d206146528da98184ed222ceba5637
Sergiogalera125 <sergio_galera@hotmail.com>
2022-12-29 16:24:32 -0300

    teste commit

--- main.py
+++ main.py
@@ -14,5 +14,4 @@ st.set_page_config(page_title="vai timao",
 excel_data_df = pd.read_excel('teste planilha.xlsx', sheet_name='Planilha1', usecols=['a', 'b','c','d','e','f'])
 print(excel_data_df)
 
-
-st.dataframe(excel_data_df)
+st.write(excel_data_df)
