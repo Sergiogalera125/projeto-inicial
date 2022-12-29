6d13a3d79331263ed81567fdd88b2c2850e7d9cf
Sergiogalera125 <sergio_galera@hotmail.com>
2022-12-29 18:38:32 -0300

    teste commit

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
-import streamlit as st
+
 import pandas as pd
-
-
+import plotly.express as px
+import streamlit as st
 
 # Reads Excel with a single time series
 # no dates column, top row with series name
-
-
 
 st.set_page_config(page_title="vai timao",
                    layout="wide")
@@ -19,9 +17,15 @@ st.image('Pedro_Fernanda_Wurlitzer.jpg')
 
 st.image('Tomas_Fernanda_Wurlitzer.jpg')
 
-df = pd.read_excel('teste planilha.xlsx', sheet_name='Planilha1')
-#print(excel_data_df)
+df = px.read_excel(
+    io='planilha.xlsx',
+    engine='openpyxl',
+    sheet_name='carro',
+    skiprows=1,
+    usecols='A:F',
+    nrows=500,
+)
 
+# print(df)
 
-
-# st.write(df)
+st.write(df)
