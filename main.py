9168f71e0398ba33cf4339667c00536972998f9d
Sergiogalera125 <sergio_galera@hotmail.com>
2022-12-29 16:19:01 -0300

    teste commit

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import pandas as pd
 # Reads Excel with a single time series
 # no dates column, top row with series name
 
+
+
 st.set_page_config(page_title="vai timao",
                    layout="wide")
 
-st.write("oi peidolina")
+excel_data_df = pd.read_excel('teste planilha.xlsx', sheet_name='Planilha1', usecols=['a', 'b','c','d','e','f'])
+print(excel_data_df)
+
+
+st.dataframe(excel_data_df)
